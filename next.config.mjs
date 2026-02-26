13b4fd84985f90c14a23638c9a348ec3eaa714cc
oguzhanacar917 <orwellg19847@gmail.com>
2026-02-26 19:17:56 +0300

    idiot popup disabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    /* config options here */
+    devIndicators: false,
 };
 
 export default nextConfig;
